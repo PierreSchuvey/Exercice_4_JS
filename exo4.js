55c80bc0f80a458c61b535795bb7a01613c783da
Pierre Schuvey <pierre.schuveye2n@gmail.com>
2017-10-24 13:43:03 +0200

    Validation exo

--- exo4.js
+++ exo4.js
@@ -1,8 +1,15 @@
 function afficher()
 {
-  var name = 'Nom : '+document.getElementById('nom').value+'\n';
-  var firstname = 'Prenom : '+document.getElementById('prenom').value+'\n';
-  var city = 'Ville : '+document.getElementById('ville').value+'\n';
-  var result= name + firstname + city;
-  alert(result);
+  var name = document.getElementById('nom').value;
+  var firstname = document.getElementById('prenom').value;
+  var city = document.getElementById('ville').value;
+  if (isNaN(name) == true && isNaN(firstname) == true && isNaN(city) && name && firstname && city){ //vérification que les données saisis sont bien des chaine de caractère
+          name = 'Nom :' + name;
+          firstname = 'Prénom :' + firstname;
+          town  = 'Ville :' + city;
+          alert(name + '\n' + firstname + '\n' + city); //affichage du message dans une box
+        }
+        else{
+          alert('Veuillez vérifier votre saisis');
+        }
 }
